c5d2680ec99a4997e433e6c0630caa18b3769460
smth-anya <100474697+smth-anya@users.noreply.github.com>
2022-03-19 11:07:22 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -3,5 +3,19 @@
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
 def gen_list(size, of, to):
-    pass
+    import random  # импорт в функции возможен, но не рекомендуется PEP-8
+    """
+    :param size: кол-во элементов списка
+    :param at: минимально возможное значение элементов
+    :param to: максимально возможное значение элементов
+    :return: списко из size произвольных элементов вдиапазоне at..to 
+    """
+    result_list = []
+    for _ in range(size):
+        result_list.append(random.randint(of, to))
+    return result_list
+
+
+my_list = gen_list(5, -100, 100)
+print(my_list)
 
