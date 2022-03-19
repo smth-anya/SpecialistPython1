ca69e7d4f9078fdb37806c8aa59802981303f01f
smth-anya <100474697+smth-anya@users.noreply.github.com>
2022-03-19 11:34:18 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,11 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def last_page(num_items, items_on_page):
+    return num_items % items_on_page
+
+print(last_page(3,3))
+print(last_page(5,3))
+print(last_page(1,3))
+print(last_page(11,3))
+print(last_page(101,4))
